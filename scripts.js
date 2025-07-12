071aa879cebe57ac506aebd2ffeaefac087ca271
SidneyGomes878 <sidneyerickson09@gmail.com>
2025-07-12 14:33:39 -0300

    js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
 const prevButtom = document.getElementById('prev')
 const nextButton = document.getElementById('next')
 const items = document.querySelectorAll('.item')
+const dots = document.querySelectorAll('.dot')
+const numberIndicator = document.querySelector('.numbers')
+const list = document.querySelector('.list')
+
+
+let active = 0;
+const total = items.length
+let timer;
+
+
+prevButtom.addEventListener('click', () => {
+    alert("prev")
+})
+
+nextButton.addEventListener('click', () => {
+    alert("next")
+})
+    
